3ce174306c44f50d16b8ef795cacb4ba07e088b4
Herbert-Lwanga <herbolwanga@gmail.com>
2023-10-29 01:56:22 +0300

    Creating the calculator user interface with buttons to a acceptable level mainly using CSS

--- script.js
+++ script.js
@@ -13,3 +13,21 @@ function multiply (num1, num2){
 function divide(num1, num2){
     return num1 / num2;
 };
+
+let firstNum;
+let operator;
+let secondNum;
+
+function operate(operator, firstNum, secondNum){
+    if(operator === "+"){
+        console.log(add(firstNum, secondNum));
+    }else if( operator === "-"){
+        console.log(subtract(firstNum, secondNum));
+
+    }else if( operator === "*"){
+        console.log(multiply(firstNum, secondNum));
+
+    }else if( operator === "/"){
+        console.log(divide(firstNum, secondNum));
+    }
+}
